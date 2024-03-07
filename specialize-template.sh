a6712d50466e96e3ed2ea9b48c2e4733a24956aa
George <GeorgeLyon@users.noreply.github.com>
2024-03-07 14:11:16 -0800

    Fix specialization workflow

--- specialize-template.sh
+++ specialize-template.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-set -euo pipefail
+set -eo pipefail
 
+NEW_PROJECT_NAME=$1
 if [[ -z "${NEW_PROJECT_NAME}" ]]; then
   echo "NEW_PROJECT_NAME is not set"
   exit 1
 fi
 
+NEW_PROJECT_BUNDLE_ID_PREFIX=$2
 if [[ -z "${NEW_PROJECT_BUNDLE_ID_PREFIX}" ]]; then
   echo "NEW_PROJECT_BUNDLE_ID_PREFIX is not set"
   exit 1
@@ -14,17 +16,25 @@ fi
 
 # `find` returns folders before their children, so this will always rename the parent folder first.
 # We re-run `find` every iteration so that the path is correct after renaming a parent folder.
+echo "Moving files..."
 while FILE=$(find . -name '*SwiftProjectTemplate*' | head -n1) && [ -n "$FILE" ]; do
   NEW_FILE=$(echo $FILE | sed -e "s/SwiftProjectTemplate/$NEW_PROJECT_NAME/")
   echo "mv \"$FILE\" \"$NEW_FILE\""
   mv "$FILE" "$NEW_FILE"
 done
 git add -A
+echo "Files moved."
 
-# Replace references to `SwiftProjectTemplate`
+echo "Replacing references to SwiftProjectTemplate..."
 git ls-files --cached --modified --others --exclude-standard -z |
-  xargs -0 sed -i '' -e "s/SwiftProjectTemplate/$NEW_PROJECT_NAME/g"
+  xargs -0 -I {} sed -i.template-specialization-backup "s/SwiftProjectTemplate/$NEW_PROJECT_NAME/g" {}
 
-# Replace bundle identifier
+echo "Replacing bundle identifier..."
 git ls-files --cached --modified --others --exclude-standard -z |
-  xargs -0 sed -i '' -e "s/com.github.georgelyon/$NEW_PROJECT_BUNDLE_ID_PREFIX/g"
+  xargs -0 -I {} sed -i.template-specialization-backup "s/com.github.georgelyon/$NEW_PROJECT_BUNDLE_ID_PREFIX/g" {}
+
+echo "Removing backup files..."
+find . -name '*.template-specialization-backup' -exec rm {} \;
+git add -A
+
+echo "Done."
